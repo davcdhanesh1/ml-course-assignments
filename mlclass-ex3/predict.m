9160f84bd5e9acaa35be704ea93965b970204aab
davcdhanesh1 <davcdhanesh1@gmail.com>
2015-01-24 11:04:49 +0530

    [ex3] predicting values by using NN parameters

--- mlclass-ex3/predict.m
+++ mlclass-ex3/predict.m
@@ -10,6 +10,12 @@ num_labels = size(Theta2, 1);
 % You need to return the following variables correctly 
 p = zeros(size(X, 1), 1);
 
+% Adding bias unit in each layer
+X = [ones(size(X,1),1) X];
+
+%Theta1 = [ones(size(Theta1,1), 1) Theta1];
+%Theta2 = [ones(size(Theta2,1), 1) Theta2];
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
 %               your learned neural network. You should set p to a 
@@ -20,16 +26,19 @@ p = zeros(size(X, 1), 1);
 %       information see 'help max'. If your examples are in rows, then, you
 %       can use max(A, [], 2) to obtain the max for each row.
 %
-
-
-
-
-
-
-
-
-
 % =========================================================================
 
+% layer1 is input layer
+layer1 = X;
 
+% layer2 is hidden layer
+layer2 = sigmoid(layer1 * (Theta1)');
+layer2BaseUnit = ones(size(layer2,1), 1);
+layer2 = [layer2BaseUnit layer2];
+
+% layer3 is output layer
+layer3 = sigmoid(layer2 * (Theta2)');
+
+[maxValue, index] = max(layer3, [], 2);
+p = index;
 end
