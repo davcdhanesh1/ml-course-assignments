60e6d3aff8331e16b9b2a42ee483b3af06b949da
davcdhanesh1 <davcdhanesh1@gmail.com>
2015-01-26 08:04:49 +0530

    [ex4] RandInitializeWeights for gradient descent

--- mlclass-ex4/randInitializeWeights.m
+++ mlclass-ex4/randInitializeWeights.m
@@ -8,9 +8,8 @@ function W = randInitializeWeights(L_in, L_out)
 %   Note that W should be set to a matrix of size(L_out, 1 + L_in) as
 %   the column row of W handles the "bias" terms
 %
-
 % You need to return the following variables correctly 
-W = zeros(L_out, 1 + L_in);
+
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Initialize W randomly so that we break the symmetry while
@@ -18,15 +17,8 @@ W = zeros(L_out, 1 + L_in);
 %
 % Note: The first row of W corresponds to the parameters for the bias units
 %
-
-
-
-
-
-
-
-
-
+epsilon_unit = 0.12;
+W = zeros(L_out, 1 + L_in) * 2 * epsilon_unit - epsilon_unit;
 % =========================================================================
 
 end
